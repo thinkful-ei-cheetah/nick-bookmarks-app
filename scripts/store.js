94087de9203a31f9f1cce01085a5a024f03ed06e
Nick Lee <leexnick@gmail.com>
2019-04-04 20:38:02 -0700

    clean up method declarations in store

--- scripts/store.js
+++ scripts/store.js
@@ -1,39 +1,40 @@
 'use strict';
 
 const store = (function() {
-  const addBookmark = function(bookmark) {
+
+  function addBookmark(bookmark) {
     this.bookmarks.push(bookmark);
-  };
+  }
 
-  const findById = function(id) {
+  function findById(id) {
     return this.bookmarks.find(bookmark => bookmark.id === id);
-  };
+  }
 
-  const findAndDelete = function(id) {
+  function findAndDelete(id) {
     this.bookmarks = this.bookmarks.filter(bookmark => bookmark.id !== id);
-  };
+  }
 
-  const findAndUpdate = function(id, newData) {
+  function findAndUpdate(id, newData) {
     const bookmarkToUpdate = this.findById(id);
     Object.assign(bookmarkToUpdate, newData);
-  };
+  }
 
-  const setBookmarkExpanded = function(id, isExpanded) {
+  function setBookmarkExpanded(id, isExpanded) {
     const bookmark = this.findById(id);
     bookmark.expanded = isExpanded;
-  };
+  }
 
-  const setUserName = function(name) {
+  function setUserName(name) {
     this.userName = name;
-  };
+  }
 
-  const setAdding = function(adding) {
+  function setAdding(adding) {
     this.adding = adding;
-  };
+  }
 
-  const setMinRating = function(minRating) {
+  function setMinRating(minRating) {
     this.minRating = minRating;
-  };
+  }
 
   return {
     // PROPERTIES
@@ -41,6 +42,7 @@ const store = (function() {
     userName: 'Nick',
     adding: false,
     minRating: 5, // Default value is 5 (display all)
+    
     // METHODS
     addBookmark,
     findById,
