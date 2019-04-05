e0651e0463d68bcc8af41e47bd9a496df08c7a68
Nick Lee <leexnick@gmail.com>
2019-04-05 13:18:36 -0700

    wire up api and add script link to html

--- scripts/api.js
+++ scripts/api.js
@@ -1,7 +1,74 @@
 'use strict';
 
+// { id:cuid(), title, url, description, rating, expanded:false, isEditing:false }
+
 const api = (function() {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/nick';
-  
-  
+
+  function createBookmark(title, url, description, rating) {
+    let newBookmark = {
+      title,
+      url,
+      description,
+      rating,
+    };
+
+    newBookmark = JSON.stringify(newBookmark);
+
+    return bookmarksApiFetch(BASE_URL + '/bookmarks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: newBookmark
+    });
+  }
+
+  function readBookmarks() {
+    return bookmarksApiFetch(BASE_URL + '/bookmarks');
+  }
+
+  function updateBookmark(id, updateData) {
+    let data = JSON.stringify(updateData);
+
+    return bookmarksApiFetch(`${BASE_URL}/bookmarks/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: data
+    });
+  }
+
+  function deleteBookmark(id) {
+    return bookmarksApiFetch(`${BASE_URL}/bookmarks/${id}`, {
+      method: 'DELETE'
+    });
+  }
+
+  function bookmarksApiFetch(...args) {
+    let error;
+
+    return fetch(...args)
+      .then(response => {
+        if(!response.ok) {
+          error = { code: response.status };
+        }
+      })
+      .then(data => {
+        if(error) {
+          error.message = data.message;
+          return Promise.reject(error);
+        }
+
+        return data;
+      });
+  }
+
+  return {
+    createBookmark,
+    readBookmarks,
+    updateBookmark,
+    deleteBookmark
+  };
 }());
