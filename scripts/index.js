34187cca61276b837daccd09b355e83ad7c3808b
Nick Lee <leexnick@gmail.com>
2019-04-04 21:43:36 -0700

    saving current test set-up in index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,15 @@
 'use strict';
 
+/* global $, store, cuid, bookmarksList */
+
 console.log('js hooked up');
+//store.bookmarks[id] = { id:cuid(), title, url, description, rating, expand:false, isEditing:false }
+
+$(document).ready(function() {
+  store.addBookmark({id: cuid(), title:'Google', url:'www.google.com', description:'World\'s best search engine', rating: 5, expanded:true, isEditing:false});
+  store.addBookmark({id: cuid(), title:'Amazon', url:'www.google.com', description:'World\'s best search engine', rating: 5, expanded:true, isEditing:false});
+  store.addBookmark({id: cuid(), title:'nix.codes', url:'www.google.com', description:'World\'s best search engine', rating: 5, expanded:true, isEditing:false});
+
+  console.log(store);
+  bookmarksList.render();
+});
